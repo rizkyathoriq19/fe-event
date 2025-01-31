8ecc7efdf351d7be9dbbb04cd7cc05592aa7189b
rizkyathoriq19 <rizkyathoriq19@gmail.com>
2025-01-31 10:33:01 +0700

    feat: init project

---
+++ src/components/commons/PageHead/PageHead.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+interface PropTypes { 
+    title?: string;
+}
+
+const PageHead = (props: PropTypes) => {
+    const { title = 'Acara' } = props;
+    return (
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>{title}</title>
+        </Head>
+    );
+};
+
+export default PageHead;
