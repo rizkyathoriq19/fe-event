a55598d6f13c2c66ca98c3192582586d932f9df0
rizkyathoriq19 <rizkyathoriq19@gmail.com>
2025-02-07 11:02:21 +0700

    feat: ui auth login

--- src/components/views/Auth/Register/Register.tsx
+++ src/components/views/Auth/Register/Register.tsx
@@ -35,8 +35,8 @@ const Register = () => {
       </div>
       <Card>
         <CardBody className="p-8">
-          <h2 className="text-xl font-bold text-danger-500">Create Account</h2>
-          <p className="mb-4 text-small">
+          <h2 className="text-2xl font-bold text-danger-500">Create Account</h2>
+          <p className="mb-4 mt-2 text-small">
             Have an Account?&nbsp;
             <Link href="/auth/login" className="font-semibold text-danger-400">
               Login Here
